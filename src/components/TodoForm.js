cdd8938fbe79c88869c577cc8de378d1435892b3
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-07 14:44:59 -0700

    form, no redux

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,5 +1,33 @@
 import React from 'react'
 
-const TodoForm = () => null
+class TodoForm extends React.Component {
+  state = { name: '' }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    //stuff
+    this.setState({name: ''})
+  }
+
+  handleChange = (e) => {
+    this.setState({ name: e.target.value })
+  }
+
+  render () {
+    const { name } = this.state
+    return (
+      <div>
+        <h3>Add a Todo</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            value={name}
+            onChange={this.handleChange}
+            required
+          />
+        </form>
+      </div>
+    )
+  }
+}
 
 export default TodoForm
