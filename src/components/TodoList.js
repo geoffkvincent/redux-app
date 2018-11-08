1a52e4260ccb9e8a38cda5b0bc9bb10f8b80ecb1
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-08 10:10:05 -0700

    filtered function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,6 +2,17 @@ import React from 'react'
 import { connect } from 'react-redux'
 import Todo from './Todo'
 
+const filtered = (todos, activeFilter) => {
+  switch(activeFilter) {
+    case 'Active':
+      return todos.filter(t => !t.complete)
+    case 'Complete':
+      return todos.filter(t => t.complete)
+    default:
+      return todos
+  }
+}
+
 const TodoList = ({ todos, activeFilter }) => (
   <ul>
     {todos.map( t =>
