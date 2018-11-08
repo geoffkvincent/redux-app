1fd9e862977b91c1a6a6bb497163b5cabba09338
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-08 11:31:36 -0700

    toggle todo action

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,7 +1,13 @@
 const ADD_TODO = 'ADD_TODO'
 const TOGGLE_TODO = 'TOGGLE_TODO'
 
+export const addTodo = (todo) => {
+  return { type: ADD_TODO, todo }
+}
 
+export const toggleTodo = (id) => {
+  return { type: TOGGLE_TODO, id}
+}
 
 const todos = (state = [], action ) => {
   switch(action.type) {
